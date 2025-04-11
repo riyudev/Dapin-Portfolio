198228d47bfabc6853619ef510427374c74e09f3
riyudev <virtudazoy@gmail.com>
2025-04-11 13:25:08 +0800

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-950">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
